3e963c0b2d9c2d0d2a34c9dd3af0d3573feca6b9
Rishabh k <rishabh.webllisto@gmail.com>
2019-08-08 18:21:00 +0530

    Changes home page

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,18 @@
 $(document).on('ready', function() {
-
+	// Header Active class function start here
+	var href = document.location.href;
+	var lastPathSegment = href.substr(href.lastIndexOf('/') + 1);
+	//alert(lastPathSegment)
+	if(lastPathSegment == 'index.html'){
+		$('#home_menue').addClass('active');
+	}else{
+		$('#home_menue').removeClass('active');
+	}
+	$('ul li a').click(function(){
+    	$('li a').removeClass("active");
+    	$(this).addClass("active");
+	});
+	// Header Active class function End here
 	// Slider start here
 	$(".lazy").slick({
     	lazyLoad: 'ondemand', // ondemand progressive anticipated
