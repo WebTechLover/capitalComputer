324eb2c02e27d33d9b245affb0b9f2e20c0a0ec4
Shrikant Dev <shrikant9907@gmail.com>
2019-08-09 22:52:55 +0530

    minor changes

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,12 @@
-$(document).on('ready', function() {
-
+jQuery('document').ready(function(){
 
 	jQuery('.slick_slider').slick();
 
-	// Header Active class function start here
-	var href = document.location.href;
-	var lastPathSegment = href.substr(href.lastIndexOf('/') + 1);
-	//alert(lastPathSegment)
-	if(lastPathSegment == 'index.html'){
-		$('#home_menue').addClass('active');
-	}else{
-		$('#home_menue').removeClass('active');
-	}
-	$('ul li a').click(function(){
-    	$('li a').removeClass("active");
-    	$(this).addClass("active");
-	});
-	// Header Active class function End here
-	
+});
+
+
+
+
  
-}); 
+
+	
